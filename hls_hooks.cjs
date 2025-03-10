10b007876497cd5987858d4dcc639e8586c787da
Harishankar Kumar <hsk@althire.ai>
2025-03-11 01:06:36 +0530

    Remove need for full client ip

--- hls_hooks.cjs
+++ hls_hooks.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   "redirect_final": function(url) {
-    return url.replace("http://localhost:4541/", "https://1346560992015814729.discordsays.com/.proxy/api/bypass?url=");
+    return url.replace("http://localhost:4541/", "/.proxy/api/bypass?url=");
   }
 };
 console.log("hls_hooks.js loaded");
