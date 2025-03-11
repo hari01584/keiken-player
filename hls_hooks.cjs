0944dced42be8450921ee1cc7fedfabeb844e05f
Harishankar Kumar <hsk@althire.ai>
2025-03-12 01:29:30 +0530

    Fix minor replacement

--- hls_hooks.cjs
+++ hls_hooks.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   "redirect_final": function(url) {
-    return url.replace("http://localhost:4541/", "/.proxy/api/bypass?url=");
+    return url.replace("http://0.0.0.0:4541/", "/.proxy/api/bypass?url=");
   }
 };
 console.log("hls_hooks.js loaded");
